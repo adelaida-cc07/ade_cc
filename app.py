4abb82736695b3487b0a7c8b7fbebd27e1102fcb
adelaida-cc07 <adecruzc04@gmail.com>
2024-11-11 20:35:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,68 @@
-# Ruta para actualizar una tarea existente
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import pusher
+import mysql.connector
+
+# Conexión a la base de datos
+con = mysql.connector.connect(
+    host="185.232.14.52",
+    database="u760464709_tst_sep",
+    user="u760464709_tst_sep_usr",
+    password="dJ0CIAFF="
+)
+
+# Inicialización de Flask
+app = Flask(__name__)
+CORS(app)
+
+# Ruta principal
+@app.route("/")
+def index():
+    return render_template("app.html")
+
+# Ruta para guardar tareas
+@app.route("/guardar", methods=["POST"])
+def alumnosGuardar():
+    Titulo = request.form["titulo"]
+    Descripcion = request.form["descripcion"]
+
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor()
+    sql = "INSERT INTO tst0_tareas (Titulo, Descripcion) VALUES (%s, %s)"
+    val = (Titulo, Descripcion)
+    cursor.execute(sql, val)
+    con.commit()
+
+    cursor.close()
+
+    # Enviar evento de Pusher
+    pusher_client = pusher.Pusher(
+        app_id="1766039",
+        key="91998889612f4dcea6e7",
+        secret="b0b6a2508a63ef44c370",
+        cluster="us2",
+        ssl=True
+    )
+    pusher_client.trigger("canalRegistroTarea", "registroEventoTareas", {"titulo": Titulo, "descripcion": Descripcion})
+
+    return jsonify({"Titulo": Titulo, "Descripcion": Descripcion})
+
+# Ruta para buscar tareas
+@app.route("/buscar")
+def buscar():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM tst0_tareas ORDER BY Id_Tarea DESC")
+    registros = cursor.fetchall()
+    cursor.close()
+
+    return jsonify(registros)
+
+# Ruta para actualizar tareas
 @app.route("/actualizar", methods=["POST"])
 def actualizar():
     id_tarea = request.form["id_tarea"]
@@ -17,7 +81,7 @@ def actualizar():
     cursor.close()
     return jsonify({"mensaje": "Tarea actualizada"})
 
-# Ruta para eliminar una tarea
+# Ruta para eliminar tareas
 @app.route("/eliminar", methods=["POST"])
 def eliminar():
     id_tarea = request.form["id_tarea"]
@@ -32,3 +96,6 @@ def eliminar():
 
     cursor.close()
     return jsonify({"mensaje": "Tarea eliminada"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
